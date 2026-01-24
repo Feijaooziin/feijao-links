867c388af62432ec8fbebcc323afac3d7911d749
feijaoozin <gonardo1234@gmail.com>
2026-01-24 05:18:06 -0300

    feat: adicionando linkedin e github

--- src/data/links.ts
+++ src/data/links.ts
@@ -6,6 +6,16 @@ export type LinkItem = {
 
 export const links: LinkItem[] = [
   {
+    name: "LinkedIn",
+    description: "Conecte-se comigo profissionalmente",
+    url: "https://linkedin.com/in/leonardo-victor-",
+  },
+  {
+    name: "GitHub",
+    description: "Veja meus projetos e contribuições",
+    url: "https://github.com/Feijaooziin",
+  },
+  {
     name: "Instagram",
     description: "Acompanhe novidades e conteúdos",
     url: "https://instagram.com/feijaostudiosdev",
